309d460939a21b44455170d5b5ccf281bc63385c
Mykyta Batrak <mykbatrak@gmail.com>
2024-01-02 18:57:56 -0600

    feat(color-scheme): Added next-themes to manage color scheme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,38 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ["./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+  darkMode: "class",
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    extend: {},
+    colors: {
+      // Gray
+      "gray-1": "var(--gray-1)",
+      "gray-2": "var(--gray-2)",
+      "gray-3": "var(--gray-3)",
+      "gray-4": "var(--gray-4)",
+      "gray-5": "var(--gray-5)",
+      "gray-6": "var(--gray-6)",
+      "gray-7": "var(--gray-7)",
+      "gray-8": "var(--gray-8)",
+      "gray-9": "var(--gray-9)",
+      "gray-10": "var(--gray-10)",
+      "gray-11": "var(--gray-11)",
+      "gray-12": "var(--gray-12)",
+      "gray-a1": "var(--gray-a1)",
+      "gray-a2": "var(--gray-a2)",
+      "gray-a3": "var(--gray-a3)",
+      "gray-a4": "var(--gray-a4)",
+      "gray-a5": "var(--gray-a5)",
+      "gray-a6": "var(--gray-a6)",
+      "gray-a7": "var(--gray-a7)",
+      "gray-a8": "var(--gray-a8)",
+      "gray-a9": "var(--gray-a9)",
+      "gray-a10": "var(--gray-a10)",
+      "gray-a11": "var(--gray-a11)",
+      "gray-a12": "var(--gray-a12)",
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
